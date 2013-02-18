74aee30f29f84f411e02118319cce962f9ea2d5a
shrkey <team@shrkey.com>
2013-02-18 23:02:24 +0000

    Rejig of menu code

--- classes/admin.remembermenot.php
+++ classes/admin.remembermenot.php
@@ -8,9 +8,9 @@ if( !class_exists( 'remembermenotadmin' ) ) {
 			//login_form
 
 			if( function_exists('is_plugin_active_for_network') && is_plugin_active_for_network('remembermenot/remembermenot.php') ) {
-				add_action('network_admin_menu', array(&$this, 'add_admin_menu'));
+				add_action('network_admin_menu', array(&$this, 'add_admin_menu'), 99);
 			} else {
-				add_action('admin_menu', array(&$this, 'add_admin_menu'));
+				add_action('admin_menu', array(&$this, 'add_admin_menu'), 99);
 			}
 
 		}
@@ -38,19 +38,47 @@ if( !class_exists( 'remembermenotadmin' ) ) {
 			global $menu, $admin_page_hooks;
 
 			if( !$this->has_parent_menu() ) {
-				add_menu_page(__('Shrkey','shrkey'), __('Shrkey','shrkey'), 'manage_options',  'shrkey', array(&$this,'handle_shrkey_dashboard'), plugins_url('remembermenot/images/lock.png'));
-				add_submenu_page('shrkey', '', '', 'manage_options', "shrkey", array(&$this,'handle_shrkey_dashboard'));
+				$hook = add_menu_page(__('Remember Me','shrkey'), __('Remember Me','shrkey'), 'manage_options',  'shrkey_rememberme', array(&$this,'handle_rememberme_admin'), plugins_url('remembermenot/images/lock.png'));
+			} else {
+				$hook = add_submenu_page('shrkey', __('Remember Me','membership'), __('Remember Me','membership'), 'manage_options', "shrkey_rememberme", array(&$this,'handle_rememberme_admin'));
 			}
 
-			$hook = add_submenu_page('shrkey', __('Remember Me','membership'), __('Remember Me','membership'), 'manage_options', "shrkey_rememberme", array(&$this,'handle_rememberme_admin'));
-
 			add_action( 'load-' . $hook, array( &$this, 'handle_rememberme_admin_header' ) );
-
 
 		}
 
 		function handle_shrkey_dashboard() {
+			?>
+			<div class="wrap">
 
+				<div id="icon-index" class="icon32"><br></div>
+				<h2><?php _e('Shrkey Dashboard', 'shrkey'); ?></h2>
+
+				<div class="welcome-panel" id="welcome-panel">
+
+					<div class="welcome-panel-content">
+						<h3><?php _e('Welcome to Shrkey', 'shrkey'); ?></h3>
+						<p class="about-description">We’ve assembled some links to get you started:</p>
+						<p></p>
+					</div>
+
+				</div>
+
+				<div id="shrkey-dashboard-widgets-wrap">
+
+					<div class="metabox-holder columns-2" id="shrkey-dashboard-widgets">
+						<div class="postbox-container">
+							<div class="meta-box-sortables ui-sortable">
+								<?php do_action( 'shrkey_dashboard_widgets' ); ?>
+							</div>
+						</div>
+					</div>
+					<div class="clear"></div>
+
+				</div>
+
+			</div>
+			<?php
 		}
 
 		function handle_rememberme_admin_header() {
