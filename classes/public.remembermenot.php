0aa0ba8ad0b0c6812d0a05081b14de07c28cc574
shrkey <team@shrkey.com>
2013-02-20 14:04:10 +0000

    Added functionality to remove remember option
    Removes remember form option
    Unsets any attempt at setting remember me via form / plugin

--- classes/public.remembermenot.php
+++ classes/public.remembermenot.php
@@ -4,25 +4,42 @@ if( !class_exists( 'remembermenotpublic' ) ) {
 
 	class remembermenotpublic {
 
-		var $rememberme = 'no';
-
 		function __construct() {
 
-			// Get the setting option
-			$this->rememberme = shrkey_get_rememberme_option( 'no' );
-
-			if( $this->rememberme == 'yes' ) {
-				// Set up our actions if we are switched on
-
-			}
-			//do_action('login_form');
+			// Add the hook into the login_form
+			add_action( 'login_form', array( &$this, 'start_login_form_cache' ), 99 );
+			// Reset any attempt to set the remember option
+			add_action( 'login_head', array( &$this, 'reset_remember_option' ), 99 );
 		}
 
 		function remembermenotpublic() {
 			$this->__construct();
 		}
 
-		function
+		function reset_remember_option() {
+
+			// Remove the rememberme post value
+			if( isset($_POST['rememberme']) ) {
+				unset( $_POST['rememberme'] );
+			}
+
+		}
+
+		function start_login_form_cache() {
+
+			ob_start( array( &$this, 'process_login_form_cache' ) );
+
+		}
+
+		function process_login_form_cache( $content ) {
+
+			$content = preg_replace( '/<p class="forgetmenot">(.*)<\/p>/', '', $content);
+			/*
+			//<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label></p>
+			*/
+			return $content;
+
+		}
 
 	}
 
--- includes/functions.php
+++ includes/functions.php
@@ -10,33 +10,3 @@ if( !function_exists( 'shrkey_on_login_page' ) ) {
 	    return in_array($GLOBALS['pagenow'], array('wp-login.php'));
 	}
 }
-
-if( !function_exists( 'shrkey_get_rememberme_option' ) ) {
-	function shrkey_get_rememberme_option( $default = false ) {
-		if(is_multisite() && function_exists('is_plugin_active_for_network') && is_plugin_active_for_network('remembermenot/remembermenot.php')) {
-			return get_site_option( 'shrkey_remembermenot', $default);
-		} else {
-			return get_option( 'shrkey_remembermenot', $default);
-		}
-	}
-}
-
-if( !function_exists( 'shrkey_update_rememberme_option' ) ) {
-	function shrkey_update_rememberme_option( $value = null ) {
-		if(is_multisite() && function_exists('is_plugin_active_for_network') && is_plugin_active_for_network('remembermenot/remembermenot.php')) {
-			return update_site_option( 'shrkey_remembermenot', $value);
-		} else {
-			return update_option( 'shrkey_remembermenot', $value);
-		}
-	}
-}
-
-if( !function_exists( 'shrkey_delete_rememberme_option' ) ) {
-	function shrkey_delete_rememberme_option( ) {
-		if(is_multisite() && function_exists('is_plugin_active_for_network') && is_plugin_active_for_network('remembermenot/remembermenot.php')) {
-			return delete_site_option( 'shrkey_remembermenot' );
-		} else {
-			return delete_option( 'shrkey_remembermenot' );
-		}
-	}
-}
