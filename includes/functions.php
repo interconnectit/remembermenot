4fce7937d718e20ea509f3bf09e5548253d41e7e
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-02-13 15:01:10 +0100

    Fix PHP notice
    Ensure that $GLOBALS['pagenow'] is set before checking for it in an array.

--- includes/functions.php
+++ includes/functions.php
@@ -8,6 +8,6 @@ if (!function_exists('is_plugin_active_for_network')) {
 if (!function_exists('shrkey_on_login_page')) {
     function shrkey_on_login_page()
     {
-        return in_array($GLOBALS['pagenow'], array('wp-login.php'));
+        return isset($GLOBALS['pagenow']) && in_array($GLOBALS['pagenow'], array('wp-login.php'));
     }
 }
